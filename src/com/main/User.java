01dc58a9c7d1b123b0f5740257882155c9c5976e
Shubh Kachhadiya <shubhkachhadiya04@gmail.com>
2024-08-31 11:59:19 +0530

    Implement create user feature

--- src/com/main/User.java
+++ src/com/main/User.java
@@ -1,5 +1,7 @@
 package com.main;
 
+import static com.validator.StringValidator.validateString;
+
 public class User {
 	
 //	User Roles in the system
@@ -11,4 +13,38 @@ public class User {
 //	Instance Variables
     private String userName;
     private Role role;
+    
+
+//  Constructor
+    public User(String userName, Role role) {
+    	validateRequiredAttributes(userName,role);
+        this.userName = userName;
+        this.role = role;
+    }
+
+//  Methods
+    
+//  To check whether the user details are NULL or not    
+    private void validateRequiredAttributes(String userName, Role role) {
+        validateString(userName, "UserName should not be null");
+        if(role == null) {
+        	throw new IllegalArgumentException("User role should not be null");
+        }
+    }
+    
+//  To retrieve the userName  
+    public String getUserName() {
+        return userName;
+    }
+
+//  To retrieve the userRole  
+    public Role getRole() {
+        return role;
+    }
+
+//  To check is user is allowed to add book in library  
+    public boolean isPermittedToAddBook() {
+        return role == Role.LIBRARIAN;
+    }
+
 }
